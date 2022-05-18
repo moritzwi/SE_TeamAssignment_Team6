38e6bcd10c928c50e9b1f8b6f09fbbd0b4d0728e
moritzwin13 <moritz.winklmair@gmx.de>
2022-05-18 20:02:11 +0900

    commit

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
- 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team6.sqlite3' 
 app.config['SECRET_KEY'] = "qwerasdf"
 
 db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.debug = True
+    db.create_all() 
+    app.run()
