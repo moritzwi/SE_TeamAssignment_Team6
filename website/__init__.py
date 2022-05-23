045403b3fcd17856bf159ed4a5c3c57d3833de9e
moritzwin13 <moritz.winklmair@gmx.de>
2022-05-23 15:13:53 +0900

    commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team6.sqlite3' 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team6.sqlite3' 
     app.config['SECRET_KEY'] = "qwerasdf"
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-        return render_template("login.html")
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        name = request.form.get('name')
+        email = request.form.get('email')
+        
+        if len(email) < 4:
+            flash("Email must be greater then 4 characters", category="error")
+        if len(username) < 5:
+            flash("Username must be greater then 5 characters", category="error")
+        elif len(name) < 2:
+            flash("Name must be greater then 2 characters", category="error")
+        elif len(password) < 8:
+            flash("Password must be greater then 8 characters", category="error")
+        else:
+            #add user to db
+            pass
+            
     return render_template("sign-up.html")
 
 
