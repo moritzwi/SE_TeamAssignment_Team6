97c5f80cc66cf4878dc13d5e3b7aafbc32fd61f2
moritzwin13 <moritz.winklmair@gmx.de>
2022-05-23 15:15:53 +0900

    commit

--- website/models.py
+++ website/models.py
@@ -1,14 +1,22 @@
+from email.policy import default
 from enum import unique
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    data = db.Column(db.DateTime(timezone=True), default=func.now())
     
-
+    
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     name = db.Column(db.String(150))
     username = db.Column(db.String(150))
+    
+class Product(db.Model):
+    
     
