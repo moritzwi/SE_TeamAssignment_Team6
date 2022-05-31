75740f1a856b955b243595d878d2908fcb92c9ed
moritzwin13 <moritz.winklmair@gmx.de>
2022-05-31 15:43:55 +0900

    add/edit product page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 
 views = Blueprint('views', __name__)
@@ -12,3 +12,15 @@ def home():
 @views.route('/shop')
 def shop():
     return render_template("shop.html", user=current_user)
+
+@views.route('/user-products')
+def user_products():
+    return render_template("user-products.html", user=current_user)
+
+@views.route('/add-product')
+def add_product():
+    return render_template("add-product.html", user=current_user)
+
+@views.route('/edit-product')
+def edit_product():
+    return render_template("edit-product.html", user=current_user)
