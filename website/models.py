6197692d777a98e32ea7775b2f37e98ca51c2d29
moritzwin13 <91183036+moritzwin13@users.noreply.github.com>
2022-06-11 18:40:10 +0900

    Update models.py - User class und follower tabelle

--- website/models.py
+++ website/models.py
@@ -5,11 +5,40 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 from datetime import datetime
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150))
     password = db.Column(db.String(150))
     products = db.relationship('products', backref='user')
+    
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+            if self.is_following(user):
+                self.followed.remove(user)
+
+    def is_following(self, user):
+            return self.followed.filter(
+                followers.c.followed_id == user.id).count() > 0
+            
+    def followed_products(self):
+            return products.query.join(
+                followers, (followers.c.followed_id == products.user_id)).filter(
+                    followers.c.follower_id == self.id)
 
     
 class products(db.Model):
